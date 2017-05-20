5fef8267af14460610f919e41dfcb674c1c58884
IsaacPerezSegura <15240485@itleon.edu.mx>
2017-05-20 18:13:10 -0500

    Create Alumno.java

---
+++ Clases/src/Alumno/Alumno.java
@@ -0,0 +1,29 @@
+package Alumno;
+public class Alumno {
+    private String nombreCompleto;
+    private String matricula;
+    private int semestre;
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+}
